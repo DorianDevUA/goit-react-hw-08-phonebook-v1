c1e423ceae5343efa563cecbfd878a60c0935656
Dorian Devua <Dorian.DevUa@gmail.com>
2024-08-01 23:17:14 +0400

    add theme for ThemeProvider

---
+++ src/constants/theme.jsx
@@ -0,0 +1,16 @@
+const theme = Object.freeze({
+  colors: {
+    white: '#ffffff',
+    black: '#010101',
+    red: '#f44336',
+    green: '#4caf50',
+    blue: '#2196f3',
+    primaryText: '#212121',
+    secondaryText: '#757575',
+    primaryBackground: '#faebd7',
+    secondaryBackground: '#2c2c2c',
+  },
+  spacing: value => `${value * 4}px`,
+});
+
+export default theme;
