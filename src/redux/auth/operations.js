90a902e9861567c3555b59399d5d32aad6c533a2
Dorian Devua <Dorian.DevUa@gmail.com>
2024-08-01 23:20:26 +0400

    add authOperations

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,13 +1,25 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://connections-api.goit.global';
+const BASE_URL = 'https://connections-api.goit.global';
+
+axios.defaults.baseURL = BASE_URL;
+
+const token = {
+  set(token) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  },
+  unset() {
+    axios.defaults.headers.common.Authorization = ``;
+  },
+};
 
 const signup = createAsyncThunk(
   'auth/signup',
   async (credentials, thunkAPI) => {
     try {
       const response = await axios.post(`/users/signup`, credentials);
+      token.set(response.data.token);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -15,8 +27,49 @@ const signup = createAsyncThunk(
   },
 );
 
+const login = createAsyncThunk('auth/login', async (credentials, thunkAPI) => {
+  try {
+    const response = await axios.post(`/users/login`, credentials);
+    token.set(response.data.token);
+    return response.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
+
+const logout = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
+  try {
+    await axios.post(`/users/logout`);
+    token.unset();
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
+
+const getUser = createAsyncThunk('auth/getCurrentUser', async (_, thunkAPI) => {
+  const state = thunkAPI.getState();
+  const persistedToken = state.auth.token;
+
+  if (persistedToken === null) {
+    console.log('Токена немає, rejectedWithValue');
+    return thunkAPI.rejectWithValue();
+  }
+
+  token.set(persistedToken);
+
+  try {
+    const response = await axios.get(`/users/current`);
+    return response.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
+
 const authOperations = {
   signup,
+  login,
+  logout,
+  getUser,
 };
 
 export default authOperations;
--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -11,11 +11,26 @@ const authSlice = createSlice({
   name: 'auth',
   initialState,
   extraReducers: builder => {
-    builder.addCase(authOperations.signup.fulfilled, (state, { payload }) => {
-      state.user = payload.user;
-      state.token = payload.token;
-      state.isLoggedIn = true;
-    });
+    builder
+      .addCase(authOperations.signup.fulfilled, (state, { payload }) => {
+        state.user = payload.user;
+        state.token = payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(authOperations.login.fulfilled, (state, { payload }) => {
+        state.user = payload.user;
+        state.token = payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(authOperations.logout.fulfilled, state => {
+        state.user = { name: null, email: null };
+        state.token = null;
+        state.isLoggedIn = false;
+      })
+      .addCase(authOperations.getUser.fulfilled, (state, { payload }) => {
+        state.user = payload;
+        state.isLoggedIn = true;
+      });
   },
 });
 
