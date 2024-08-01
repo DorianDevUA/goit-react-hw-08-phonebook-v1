46919f30da09528fa98ca5bc1012fab17472a3e2
Dorian Devua <Dorian.DevUa@gmail.com>
2024-08-01 20:00:46 +0400

    change base name

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: 'goit-react-hw-08-phonebook',
+  base: 'goit-react-hw-08-phonebook-v1',
   build: {
     sourcemap: true,
   },
