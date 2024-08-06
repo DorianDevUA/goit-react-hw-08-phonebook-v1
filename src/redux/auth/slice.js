24f0bdfd3c55893f7cb831dbb41bdf8d31fc421b
Dorian Devua <Dorian.DevUa@gmail.com>
2024-08-06 22:12:00 +0400

    add extraReducers

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -28,9 +28,16 @@ const authSlice = createSlice({
         state.token = null;
         state.isLoggedIn = false;
       })
+      .addCase(authOperations.getUser.pending, state => {
+        state.isRefreshing = true;
+      })
       .addCase(authOperations.getUser.fulfilled, (state, { payload }) => {
         state.user = payload;
         state.isLoggedIn = true;
+        state.isRefreshing = false;
+      })
+      .addCase(authOperations.getUser.rejected, state => {
+        state.isRefreshing = false;
       });
   },
 });
