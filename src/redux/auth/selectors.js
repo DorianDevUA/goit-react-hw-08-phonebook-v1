767351e6d6b1e4a031fd10cb95401ed4ecf001e2
Dorian Devua <Dorian.DevUa@gmail.com>
2024-08-06 21:48:44 +0400

    add isRefreshing to state for authSlice

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -3,6 +3,7 @@ const selectUserName = state => state.auth.user.name;
 const selectUserEmail = state => state.auth.user.email;
 const selectToken = state => state.auth.token;
 const selectIsLoggedIn = state => state.auth.isLoggedIn;
+const selectIsRefreshing = state => state.auth.isRefreshing;
 
 const authSelectors = {
   selectUser,
@@ -10,6 +11,7 @@ const authSelectors = {
   selectUserEmail,
   selectToken,
   selectIsLoggedIn,
+  selectIsRefreshing,
 };
 
 export default authSelectors;
