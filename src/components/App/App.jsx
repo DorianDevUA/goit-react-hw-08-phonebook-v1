0391cd1382ccec524eccc5b3c7f3c2d921d55f2b
Dorian Devua <Dorian.DevUa@gmail.com>
2024-08-06 22:12:52 +0400

    add Refreshing page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,49 +1,48 @@
+import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Layout from 'components/Layout';
+import { useDispatch } from 'react-redux';
+import { authOperations } from 'redux/auth';
+import { useAuth } from 'hooks';
 import HomeView from 'pages/HomeView';
 import PhonebookView from 'pages/PhonebookView';
 import RegisterView from 'pages/RegisterView';
 import LoginView from 'pages/LoginView';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { authOperations } from 'redux/auth';
+import Layout from 'components/Layout';
 import RestrictedRoute from 'components/RestrictedRoute';
 import PrivateRoute from 'components/PrivateRoute';
 
 const App = () => {
   const dispatch = useDispatch();
+  const { isRefreshing } = useAuth();
 
   useEffect(() => {
     dispatch(authOperations.getUser());
   }, [dispatch]);
 
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomeView />} />
-          <Route
-            path="contacts"
-            element={<PrivateRoute component={PhonebookView} />}
-          />
-          <Route
-            path="register"
-            element={
-              <RestrictedRoute
-                component={RegisterView}
-                redirectTo="/contacts"
-              />
-            }
-          />
-          <Route
-            path="login"
-            element={
-              <RestrictedRoute component={LoginView} redirectTo="/contacts" />
-            }
-          />
-        </Route>
-      </Routes>
-    </>
+  return isRefreshing ? (
+    <b>Refreshing page...</b>
+  ) : (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomeView />} />
+        <Route
+          path="contacts"
+          element={<PrivateRoute component={PhonebookView} />}
+        />
+        <Route
+          path="register"
+          element={
+            <RestrictedRoute component={RegisterView} redirectTo="/contacts" />
+          }
+        />
+        <Route
+          path="login"
+          element={
+            <RestrictedRoute component={LoginView} redirectTo="/contacts" />
+          }
+        />
+      </Route>
+    </Routes>
   );
 };
 
