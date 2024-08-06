084a5cc3c70d69111e3b2f306ea753582a74e51c
Dorian Devua <Dorian.DevUa@gmail.com>
2024-08-06 22:19:51 +0400

    added a condition to render the Phonebook link

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,15 +1,20 @@
 import { NavLink } from 'react-router-dom';
+import { useAuth } from 'hooks';
 
 const Navigation = () => {
+  const { isLoggedIn } = useAuth();
+
   return (
     <div>
       <ul>
         <li>
           <NavLink to="/">Home</NavLink>
         </li>
-        <li>
-          <NavLink to="contacts">Phonebook</NavLink>
-        </li>
+        {isLoggedIn && (
+          <li>
+            <NavLink to="contacts">Phonebook</NavLink>
+          </li>
+        )}
       </ul>
     </div>
   );
