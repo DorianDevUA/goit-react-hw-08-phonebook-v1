c378421ea438636a9d3c565ddbd4ce6a8909fbe0
Dorian Devua <Dorian.DevUa@gmail.com>
2024-08-13 00:53:41 +0400

    add render ContactList component

--- src/pages/PhonebookPage.jsx
+++ src/pages/PhonebookPage.jsx
@@ -1,7 +1,22 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useContacts } from 'hooks';
+import { contactsOperations } from 'redux/contacts';
+import ContactList from 'components/ContactList';
+
 const PhonebookPage = () => {
+  const dispatch = useDispatch();
+  const { contacts } = useContacts();
+  const contactsExist = contacts.length > 0;
+
+  useEffect(() => {
+    dispatch(contactsOperations.getAll());
+  }, [dispatch]);
+
   return (
     <>
-      <div>PhonebookPage</div>;
+      <div>toDo filterByName and filteredContacts</div>
+      {contactsExist && <ContactList contacts={contacts} />}
     </>
   );
 };
