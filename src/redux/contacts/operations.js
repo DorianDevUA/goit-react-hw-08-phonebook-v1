4341c390783a0e3a8999d7d9f1a820660a874ba1
Dorian Devua <Dorian.DevUa@gmail.com>
2024-08-12 21:59:10 +0400

    add operations

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -10,8 +10,50 @@ const getAll = createAsyncThunk('contacts/getAll', async (_, thunkAPI) => {
   }
 });
 
+const createContact = createAsyncThunk(
+  'contacts/create',
+  async (contact, thunkAPI) => {
+    try {
+      const { data } = await axios.post(`/contacts`, contact);
+      console.log(data);
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  },
+);
+
+const deleteContact = createAsyncThunk(
+  'contacts/delete',
+  async (contactId, thunkAPI) => {
+    try {
+      const response = await axios.delete(`/contacts/${contactId}`);
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  },
+);
+
+const updateContact = createAsyncThunk(
+  'contacts/update',
+  async ({ contactId, updates }, thunkAPI) => {
+    try {
+      const response = await axios.patch(`/contacts/${contactId}`, updates);
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  },
+);
+
 const contactsOperations = {
   getAll,
+  createContact,
+  deleteContact,
+  updateContact,
 };
 
 export default contactsOperations;
