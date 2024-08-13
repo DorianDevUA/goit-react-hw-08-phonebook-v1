c3ba4f1acf8caf0081e11ccb30265437f23e08e9
Dorian Devua <Dorian.DevUa@gmail.com>
2024-08-13 23:48:29 +0400

    add setFilter action

--- src/redux/filter/slice.js
+++ src/redux/filter/slice.js
@@ -3,7 +3,13 @@ import { createSlice } from '@reduxjs/toolkit';
 const filterSlice = createSlice({
   name: 'filter',
   initialState: '',
-  reducers: {},
+  reducers: {
+    setFilter: (state, action) => {
+      return (state = action.payload);
+    },
+  },
 });
+
+export const { setFilter } = filterSlice.actions;
 
 export default filterSlice.reducer;
