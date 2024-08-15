7b184f7e2f5f34572da354ed6569cdf46f503cf4
Dorian Devua <Dorian.DevUa@gmail.com>
2024-08-15 14:00:23 +0400

    add showCreateContactModal state

--- src/pages/PhonebookPage.jsx
+++ src/pages/PhonebookPage.jsx
@@ -1,11 +1,16 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useFilter } from 'hooks';
 import { contactsOperations } from 'redux/contacts';
+import IconButton from 'components/IconButton';
 import ContactFilter from 'components/ContactFilter';
 import ContactList from 'components/ContactList';
+import { MdAdd } from 'react-icons/md';
+import Modal from 'components/Modal';
+import ContactForm from 'components/ContactForm';
 
 const PhonebookPage = () => {
+  const [showCreateContactModal, setShowCreateContactModal] = useState(false);
   const dispatch = useDispatch();
   const { filteredContacts: contacts } = useFilter();
   const isContactsExist = contacts.length > 0;
@@ -14,10 +19,27 @@ const PhonebookPage = () => {
     dispatch(contactsOperations.getAll());
   }, [dispatch]);
 
+  const toggleCreateContactModal = () => {
+    setShowCreateContactModal(!showCreateContactModal);
+  };
+
   return (
     <>
+      <IconButton
+        type="button"
+        title="Add new contact"
+        onClick={toggleCreateContactModal}
+        aria-label="Add new contact"
+      >
+        <MdAdd size={24} />
+      </IconButton>
       <ContactFilter />
       {isContactsExist && <ContactList contacts={contacts} />}
+      {showCreateContactModal && (
+        <Modal title="Create contact" onClose={toggleCreateContactModal}>
+          <ContactForm />
+        </Modal>
+      )}
     </>
   );
 };
